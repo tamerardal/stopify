d565e9699859dc55aaa63444c240516272c6a805
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 10:58:01 +0300

    container ve title styleları oluşturuldu.

--- src/components/SongCard.styles.js
+++ src/components/SongCard.styles.js
@@ -1,3 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.5,
+    padding: 5,
+    margin: 5,
+  },
+  cardTitle: {
+    fontWeight: '600',
+  },
+});
