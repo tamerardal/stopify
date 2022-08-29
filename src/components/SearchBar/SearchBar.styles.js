abcb4211675a9ebe8aca7441002da89de55d9e4d
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 16:07:53 +0300

    searchBar stil oluşturuldu.

--- src/components/SearchBar/SearchBar.styles.js
+++ src/components/SearchBar/SearchBar.styles.js
@@ -1,3 +1,18 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    borderWidth: 0.1,
+    borderColor: 'gray',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    borderRadius: 25,
+    backgroundColor: 'lightgray',
+    height: 40,
+    elevation: 5,
+  },
+  text: {
+    paddingLeft: 10,
+  },
+});
