228188651af2d1cb54e2897ce3378ca908aa863e
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 10:26:04 +0300

    flatlist taslak oluşturuldu.

--- src/components/SongCardFlatList.js
+++ src/components/SongCardFlatList.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import {FlatList} from 'react-native';
+import SongCard from './SongCard';
+import songData from '../music-data.json';
+
+const SongCardFlatList = song => {
+  const renderSong = ({item}) => {
+    <SongCard song={item} />;
+  };
+  const renderKey = item => {
+    item.id.toString();
+  };
+
+  return (
+    <FlatList
+      data={songData}
+      renderItem={renderSong}
+      keyExtractor={renderKey}
+    />
+  );
+};
