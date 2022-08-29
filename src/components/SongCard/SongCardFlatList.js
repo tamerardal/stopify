f3176fc801be034e58f2bfb90846a7eca726194e
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 17:22:59 +0300

    filtered search bar tamamlandı.

--- src/components/SongCard/SongCardFlatList.js
+++ src/components/SongCard/SongCardFlatList.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, View, StyleSheet, SafeAreaView} from 'react-native';
 import SongCard from './SongCard';
 import songData from '../../music-data.json';
 import SearchBar from '../SearchBar/SearchBar';
 
 const SongCardFlatList = song => {
+  const [filter, setFilter] = useState(songData);
   const renderSong = ({item}) => <SongCard song={item} />;
   const renderKey = item => item.id.toString();
   const renderSeperator = () => <View style={styles.seperator} />;
+  const handleSearch = text => {
+    const filteredList = songData.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+    setFilter(filteredList);
+  };
 
   return (
     <SafeAreaView>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
       <FlatList
-        data={songData}
+        data={filter}
         renderItem={renderSong}
         keyExtractor={renderKey}
         ListHeaderComponent={() => {}}
