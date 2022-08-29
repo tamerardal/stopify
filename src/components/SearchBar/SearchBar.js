01e3aa975802a803b5a2c53194fb7d3816f3a43b
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 15:48:28 +0300

    search bar eklendi.

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import styles from './SearchBar.styles';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <TextInput>Search..</TextInput>
+    </View>
+  );
+};
+
+export default SearchBar;
