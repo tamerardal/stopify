f0057b2a3daba909687c3a97242c6a4b8a594d51
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 10:58:13 +0300

    style eklendi.

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styles from './SongCard.styles';
 
 const SongCard = ({song}) => {
   return (
-    <View>
-      <Text>{song.title}</Text>
+    <View style={styles.cardContainer}>
+      <Text style={styles.cardTitle}>{song.title}</Text>
+      <Text>{song.artist}</Text>
     </View>
   );
 };
