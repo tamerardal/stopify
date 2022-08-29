120f523c284c786ae0828ee29e830c7cf4e06799
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-08-29 10:31:13 +0300

    app taslak oluşturuldu.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, SafeAreaView, Flatlist, StyleSheet} from 'react-native';
+import songData from './music-data.json';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
